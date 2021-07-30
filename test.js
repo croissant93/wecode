df89de5dfaf00cc8aa20bbe7826248e5df58eb81
croissant93 <croissant9393@gmail.com>
2021-07-30 11:44:07 +0900

    Add: test.js 파일추가 - 0~50 짝수 구하기 완료

---
+++ test.js
@@ -0,0 +1,19 @@
+const maxNum = 50;
+
+
+function getEvenNum(num) {
+  const result = [];
+  for(i=0; i<=num; i++)  {
+    if(i%2 == 0)  {
+      console.log(i);
+      result.push(i);
+    }
+     
+      
+  }
+  
+  return = result;
+}
+
+
+getEvenNum(maxNum);
