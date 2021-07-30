a277e580a8db4155027e5645890479aa8cacf788
croissant93 <croissant9393@gmail.com>
2021-07-30 11:50:44 +0900

    Add: test.js 파일 완료

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ const maxNum = 50;
 
 function getEvenNum(num) {
   const result = [];
-  for(i=0; i<=num; i++)  {
+  for(i=1; i<=num; i++)  {
     if(i%2 == 0)  {
       console.log(i);
       result.push(i);
